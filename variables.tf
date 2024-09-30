353043c81a6dfe2dcdd92403e1b438079047f049
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2024-09-30 14:25:01 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 #resource_group
 variable "resource_group_name" {
     type = string
+    default = myvarrg
     description = "resource_group_name"
 }
 
 variable "location" {
     type = string
+    default = myvarlocation
     description = "location"
 }
