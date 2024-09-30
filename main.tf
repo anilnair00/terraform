6c83c4b84dc120a844464352c56ff844eed9a544
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2024-09-30 14:44:54 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = mytestrg
+  location = eastus2
 }
