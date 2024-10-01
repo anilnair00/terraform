82efdab4430a42b0cb55e054d46cdd1cdcfaa4e0
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2024-10-01 14:01:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,11 @@ terraform {
 # Provider Block
 provider "azurerm" {
   features {}
+  
   subscription_id = "ee902f6d-01a9-4a32-8c5f-af9403e6c766"
+  client_id       = "084f318f-40aa-4d40-93ea-5ed3472b66bc"
+  client_secret   = "UxS8Q~xZ6gqVHJlJlDbvM~-3BpEWwMmFpetXaaLD"
+  tenant_id       = "abec7981-3822-4685-98b0-533aca20c2ed"
 }
 
 resource "azurerm_resource_group" "example" {
