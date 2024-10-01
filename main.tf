b32a467293fb004ac91466b51a30929318925245
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2024-10-01 08:32:27 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,18 +12,7 @@ terraform {
 provider "azurerm" {
   features {}
 }
-resource "null_resource" "install_azure_cli" {
-  provisioner "local-exec" {
-    command = <<EOT
-      if ! az --version >nul 2>&1; then
-        echo "Azure CLI not found. Installing..."
-        powershell -Command "Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -ArgumentList '/I AzureCLI.msi /quiet' -Wait; Remove-Item .\AzureCLI.msi"
-      else
-        echo "Azure CLI is already installed."
-      fi
-    EOT
-  }
-}
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
