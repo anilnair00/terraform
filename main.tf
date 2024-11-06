8cbdf78b223f43e8621268b0bb106a4fa690fa12
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2024-11-06 07:57:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>3.0.0"
+      version = "~>4.3.0"
     }
   }
+  backend "azurerm" {}
 }
 # Provider Block
 provider "azurerm" {
